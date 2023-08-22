794d64233c67adec9a7680f9c663231ef6cd4fb1
RichestHumanAlive <walteroprimero@gmail.com>
2023-08-22 20:42:01 +0100

    Ajout d'un analyseur de corps de requête HTTP au format JSON et de routes http pour le traitement des tags

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-
 const port = 3000;
 
+const tagsRoute = require('./routes/tags');
+
+
+// Middleware Body Parser pour analyser les données JSON
+app.use(bodyParser.json());
+
+// Utilisation de la route pour les tags dans l'application
+app.use('/tags', tagsRoute);
+
 app.listen(port, () => console.log(`Serveur en écoute sur le port ${port}`));
-
-
-
