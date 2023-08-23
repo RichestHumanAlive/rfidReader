ec2027712e2e44c54acb1351a13411db5babcd69
RichestHumanAlive <walteroprimero@gmail.com>
2023-08-23 06:26:44 +0100

    Ajout d'un module de connexion à la base de données mysql

---
+++ db.js
@@ -0,0 +1,24 @@
+const mysql  = require('mysql2');
+require('dotenv').config();
+
+
+// Configuration de la connexion à la base de données
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+
+db.connect(err => {
+    if (err) {
+        console.error('Erreur de connexion à la base de données :', err);
+        return;
+    }
+    console.log('Connecté à la base de données MySQL.');
+});
+
+
+module.exports = db;
